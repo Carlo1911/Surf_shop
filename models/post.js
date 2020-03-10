dc3b65ae966410104637bf39794bf8794b602276
Carlo Alva <carlo.alva@wunderman.com>
2020-03-10 11:53:17 -0500

    post model

--- models/post.js
+++ models/post.js
@@ -1,12 +1,24 @@
-/*
-Post:
-- title - string
-- price -string
-- description - string
-- images - array o strings
-- lat - string
-- lng - string
-- location - string
-- author - object id ref. User
-- reviews - array of objects ref. Review
-*/
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    images: [String],
+    location: String,
+    lat: Number,
+    lng: Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+module.exports = mongoose.model('Post', PostSchema);
