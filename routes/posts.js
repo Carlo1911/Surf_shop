fc37e848966e830da0f5b76168498e9d5559ca19
Carlo Alva <carlo.alva@wunderman.com>
2020-03-11 14:45:14 -0500

    create post

--- routes/posts.js
+++ routes/posts.js
@@ -1,38 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const {
+  errorHandler
+} = require('../middleware');
+const {
+  getPosts,
+  newPost,
+  createPost
+} = require('../controllers/posts');
 
 /* GET posts index /posts. */
-router.get('/', (req, res, next)  => {
-  res.send('/posts');
-});
+router.get('/', errorHandler(getPosts));
 
 /* GET posts new /posts/new. */
-router.get('/new', (req, res, next)  => {
-  res.send('/posts/new');
-});
+router.get('/new', newPost);
 
 /* POST posts create /posts. */
-router.post('/', (req, res, next)  => {
-  res.send('/posts/create');
-});
+router.post('/', errorHandler(createPost));
 
 /* GET posts show /posts/:id. */
-router.get('/:id', (req, res, next)  => {
+router.get('/:id', (req, res, next) => {
   res.send('/posts/show');
 });
 
 /* GET posts edit /posts/:id/edit. */
-router.get('/:id/edit', (req, res, next)  => {
+router.get('/:id/edit', (req, res, next) => {
   res.send('/posts/edit');
 });
 
 /* PUT posts update /posts/:id. */
-router.put('/:id', (req, res, next)  => {
+router.put('/:id', (req, res, next) => {
   res.send('/posts/update');
 });
 
 /* DELETE posts destroy /posts/:id. */
-router.delete('/:id', (req, res, next)  => {
+router.delete('/:id', (req, res, next) => {
   res.send('/posts/destroy');
 });
 
