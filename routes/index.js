f4c6d8e1bb71dd7fc00ca00c69b738183de8e4f2
Carlo Alva <carlo.alva@wunderman.com>
2020-03-11 10:03:35 -0500

    login & logout

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,72 @@
 const express = require('express');
 const router = express.Router();
-const { postRegister } = require('../controllers/index');
+const passport = require('passport');
+const {
+  postRegister
+} = require('../controllers');
+const {
+  errorHandler
+} = require('../middleware');
 
 /* GET home page. */
-router.get('/', (req, res, next)  => {
-  res.render('index', { title: 'Surf Shop - Home' });
+router.get('/', (req, res, next) => {
+  res.render('index', {
+    title: 'Surf Shop - Home'
+  });
 });
 
 /* GET /register. */
-router.get('/register', (req, res, next)  => {
+router.get('/register', (req, res, next) => {
   res.send('GET /register');
 });
 
 /* POST /register. */
-router.post('/register', postRegister);
+router.post('/register', errorHandler(postRegister));
 
 /* GET /login. */
-router.get('/login', (req, res, next)  => {
+router.get('/login', (req, res, next) => {
   res.send('GET /login');
 });
 
 /* POST /login. */
-router.post('/login', (req, res, next)  => {
-  res.send('POST /login');
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+/* GET /logout. */
+router.get('/logout', (req, res, next) => {
+  req.logout();
+  res.redirect('/');
 });
 
 /* GET /profile. */
-router.get('/profile', (req, res, next)  => {
+router.get('/profile', (req, res, next) => {
   res.send('GET /profile');
 });
 
 /* PUT /profile/:user_id. */
-router.put('/profile/:user_id', (req, res, next)  => {
+router.put('/profile/:user_id', (req, res, next) => {
   res.send('POST /profile/:user_id');
 });
 
 /* GET /forgot-password. */
-router.get('/forgot-password', (req, res, next)  => {
+router.get('/forgot-password', (req, res, next) => {
   res.send('GET /forgot-password');
 });
 
 /* PUT /forgot-password. */
-router.put('/forgot-password', (req, res, next)  => {
+router.put('/forgot-password', (req, res, next) => {
   res.send('PUT /forgot-password');
 });
 
 /* GET /reset-password. */
-router.get('/reset-password/:token', (req, res, next)  => {
+router.get('/reset-password/:token', (req, res, next) => {
   res.send('GET /reset-password/:token');
 });
 
 /* PUT /reset-password. */
-router.put('/reset-password/:token', (req, res, next)  => {
+router.put('/reset-password/:token', (req, res, next) => {
   res.send('PUT /reset-password/:token');
 });
 
